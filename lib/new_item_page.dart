ddc7cc77f473b9f3243ec4c9f4de80fb19638efc
Fukunga <momongast@gmail.com>
2023-03-10 05:17:49 +0900

    新規アイテム登録ページのカルーセル作り出し

--- lib/new_item_page.dart
+++ lib/new_item_page.dart
@@ -1,26 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:qlogi/itemlist.dart';
 
-//
-// 画面 A
-//
+/// 馬のカード Widget
+class HorseCard extends StatelessWidget {
+  const HorseCard({
+    super.key,
+    required this.model,
+  });
+  // データが入ったモデル
+  final S_Model model;
+
+  @override
+  Widget build(BuildContext context) {
+    // 画像
+    final img = SizedBox(
+      height: 100,
+      child: Image.asset(
+        '${model.ItemImage}',
+      ),
+    );
+
+    // 名前
+    final text = Text(
+      model.ItemName,
+      style: const TextStyle(fontSize: 20),
+    );
+
+    // 画像と名前を縦に並べる
+    final imgAndText = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        img,
+        text,
+      ],
+    );
+
+    // カード部分を作るコンテナ
+    return Container(
+      decoration: BoxDecoration(
+        // 色
+        color: Colors.orange,
+        // 角丸
+        borderRadius: BorderRadius.circular(20),
+        // 影
+        boxShadow: [
+          BoxShadow(
+            // 影の設定
+            color: Colors.black.withOpacity(0.2), //色
+            spreadRadius: 2, // 広がりの大きさ
+            blurRadius: 2, // ぼかしの強さ
+            offset: const Offset(0, 2), // 方向
+          ),
+        ],
+      ),
+      child: imgAndText,
+    );
+  }
+}
+
+// モデル => ウィジェット に変換する関数
+Widget modelToWidget(S_Model model) {
+  // ページの部分
+  return Container(
+    // カードの周りに 10 ずつスペースを空ける
+    padding: const EdgeInsets.all(10),
+    // 中身は カード
+    child: HorseCard(model: model),
+  );
+  // カードを使う
+}
+
 class NewAddItemPage extends StatelessWidget {
   const NewAddItemPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // カルーセル
+    final carousel = PageView.builder(
+      // カルーセルのコントローラー
+      controller: PageController(
+        // 左右のカードがどのくらい見えるかのバランスを決める
+        viewportFraction: 0.6,
+      ),
+      // カードの数 = モデルの数
+      itemCount: models.length,
+      // モデルをWidgetに変換する関数
+      itemBuilder: (c, i) => modelToWidget(models[i]),
+    );
+
     return Scaffold(
       body: Container(
         color: Colors.white,
-        child: const Center(
-          child: Text(
-            '新規登録画面',
-            style: TextStyle(
-              // 文字の大きさ
-              fontSize: 20,
-              // 文字の太さ
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+        child: Column(
+          children: [
+            carousel,
+          ],
         ),
       ),
     );
